c4f7ef4506e646b9d6804ea81eb70e5866f2d8bd
Mateo Quisilema <juan.quisilema@epn.edu.ec>
2025-07-18 10:02:23 -0500

    Agregar interfaz segmentada parte 2

---
+++ src/main/java/org/epn/presentacion/MainGeneralGUI.java
@@ -0,0 +1,23 @@
+package org.epn.presentacion;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainGeneralGUI extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGeneralGUI.class.getResource("Splash.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("Sistema de Gestión - Cargando...");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/org/epn/presentacion/SplashController.java
@@ -0,0 +1,4 @@
+package org.epn.presentacion;
+
+public class SplashController {
+}
