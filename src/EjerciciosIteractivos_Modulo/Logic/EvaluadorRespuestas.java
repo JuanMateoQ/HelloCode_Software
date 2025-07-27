a5ca086cf9af6692e6fa344fe205b35c087123a5
Luis <luis.guerrero02@epn.edu.ec>
2025-07-26 21:56:27 -0500

    "Upd: Cambios en la rama main upd: usuarios, controlers"

--- src/EjerciciosIteractivos_Modulo/Logic/EvaluadorRespuestas.java
+++
@@ -1,7 +0,0 @@
-package EjerciciosIteractivos_Modulo.Logic;
-
-import java.util.ArrayList;
-
-public interface EvaluadorRespuestas {
-    ResultadoDeEvaluacion evaluarRespuestas(ArrayList<Respuesta>respuestasUsuario);
-}
